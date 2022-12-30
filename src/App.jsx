5e06124131acc962458c1ab3cc095b7d6d4306e4
Divine Edwin <edwindivine5@gmail.com>
2022-12-30 02:16:50 +0100

    added a validation
    validated the checkbox option

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useForm } from "react-hook-form";
-import { Email_Regex_Validation, Password_Regex_Validation, Phone_Regex_Validation } from "./lib";
+import {
+  Email_Regex_Validation,
+  Password_Regex_Validation,
+  Phone_Regex_Validation,
+} from "./lib";
 import "./App.css";
 
 const App = () => {
-
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
 
   return (
     <>
@@ -22,7 +30,9 @@ const App = () => {
               id="first_name"
               {...register("firstName", { required: true, minLength: 3 })}
             />
-            {errors.firstName && (<p className="error">Minimum of 3 characters</p>)}
+            {errors.firstName && (
+              <p className="error">Minimum of 3 characters</p>
+            )}
           </div>
 
           <div className="form-flex-1">
@@ -35,7 +45,9 @@ const App = () => {
               id="last_name"
               {...register("lastName", { required: true, minLength: 3 })}
             />
-            {errors.lastName && (<p className="error">Minimum of 3 characters</p>)}
+            {errors.lastName && (
+              <p className="error">Minimum of 3 characters</p>
+            )}
           </div>
         </section>
 
@@ -47,9 +59,12 @@ const App = () => {
             placeholder="+234"
             className="txt-field"
             id="phone"
-            {...register("phone", { required: true, pattern: Phone_Regex_Validation })}
+            {...register("phone", {
+              required: true,
+              pattern: Phone_Regex_Validation,
+            })}
           />
-          {errors.phone && (<p className="error">Not a valid Phone Number</p>)}
+          {errors.phone && <p className="error">Not a valid Phone Number</p>}
         </section>
 
         <section className="form-flex-1">
@@ -60,11 +75,14 @@ const App = () => {
             placeholder="yourname@gmail.com"
             className="email-field"
             id="email"
-            {...register("email", { required: true, pattern: Email_Regex_Validation })}
+            {...register("email", {
+              required: true,
+              pattern: Email_Regex_Validation,
+            })}
           />
-          {errors.email && (<p className="error">Not a valid email format</p>)}
+          {errors.email && <p className="error">Not a valid email format</p>}
         </section>
-        
+
         <section className="form-flex-1">
           <label className="form-label">Password</label>
           <input
@@ -73,9 +91,17 @@ const App = () => {
             placeholder="********"
             className="email-field"
             id="phone"
-            {...register("password", { required: true, pattern: Password_Regex_Validation })}
+            {...register("password", {
+              required: true,
+              pattern: Password_Regex_Validation,
+            })}
           />
-          {errors.password && (<p className="error">Password must contain at least a digit, special symbol, Uppercase, Lowercase and 8-10 characters long.</p>)}
+          {errors.password && (
+            <p className="error">
+              Password must contain at least a digit, special symbol, Uppercase,
+              Lowercase and 8-10 characters long.
+            </p>
+          )}
         </section>
 
         <section className="form-flex-1">
@@ -87,26 +113,32 @@ const App = () => {
             placeholder="Send me a message and I will reply you as soon as possible..."
             {...register("message", { required: true, minLength: 10 })}
           ></textarea>
-          {errors.message && (<p className="error">Characters too short</p>)}
+          {errors.message && <p className="error">Characters too short</p>}
         </section>
 
-        <section className="check-box-input">
-          <input
-            type="checkbox"
-            className="check-box"
-            name="checkbox"
-            id="checkbox"
-          />
-          <label className="form-label check-box-label">
-            You agree to providing your data to Divine Edwin, who may contact
-            you.
-          </label>
+        <section className="form-flex-1">
+          <div className="check-box-input">
+            <input
+              type="checkbox"
+              className="check-box"
+              name="checkbox"
+              id="checkbox"
+              {...register("checkbox", { required: true })}
+            />
+            <label className="form-label check-box-label">
+              You agree to providing your data to Divine Edwin, who may contact
+              you.
+            </label>
+          </div>
+          {errors.checkbox && <p className="error">Please Tick</p>}
         </section>
 
         <section>
           <button
             type="submit"
-            className={Object.keys(errors).length === 0 ? "form-btn-1" : "form-btn"}
+            className={
+              Object.keys(errors).length === 0 ? "form-btn-1" : "form-btn"
+            }
             id="submit_btn"
           >
             Send message
