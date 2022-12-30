840a5011dafc3b60511e93332e086d69a05062f7
Divine Edwin <edwindivine5@gmail.com>
2022-12-30 01:58:48 +0100

    Initial commit
    form validation with error messages using react hook form.

---
+++ src/App.jsx
@@ -0,0 +1,120 @@
+import { useForm } from "react-hook-form";
+import { Email_Regex_Validation, Password_Regex_Validation, Phone_Regex_Validation } from "./lib";
+import "./App.css";
+
+const App = () => {
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  return (
+    <>
+      <form className="form-item" onSubmit={handleSubmit(onSubmit)}>
+        <h1> Form Validation with React Hook Form</h1>
+        <section className="form-flex">
+          <div className="form-flex-1">
+            <label className="form-label">First Name</label>
+            <input
+              type="text"
+              name="firstName"
+              placeholder="Enter your first name"
+              className="txt-field"
+              id="first_name"
+              {...register("firstName", { required: true, minLength: 3 })}
+            />
+            {errors.firstName && (<p className="error">Minimum of 3 characters</p>)}
+          </div>
+
+          <div className="form-flex-1">
+            <label className="form-label">Last Name</label>
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Enter your last name"
+              className="txt-field"
+              id="last_name"
+              {...register("lastName", { required: true, minLength: 3 })}
+            />
+            {errors.lastName && (<p className="error">Minimum of 3 characters</p>)}
+          </div>
+        </section>
+
+        <section className="form-flex-1">
+          <label className="form-label">Phone Number</label>
+          <input
+            type="text"
+            name="phone"
+            placeholder="+234"
+            className="txt-field"
+            id="phone"
+            {...register("phone", { required: true, pattern: Phone_Regex_Validation })}
+          />
+          {errors.phone && (<p className="error">Not a valid Phone Number</p>)}
+        </section>
+
+        <section className="form-flex-1">
+          <label className="form-label">Email Address</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="yourname@gmail.com"
+            className="email-field"
+            id="email"
+            {...register("email", { required: true, pattern: Email_Regex_Validation })}
+          />
+          {errors.email && (<p className="error">Not a valid email format</p>)}
+        </section>
+        
+        <section className="form-flex-1">
+          <label className="form-label">Password</label>
+          <input
+            type="text"
+            name="password"
+            placeholder="********"
+            className="email-field"
+            id="phone"
+            {...register("password", { required: true, pattern: Password_Regex_Validation })}
+          />
+          {errors.password && (<p className="error">Password must contain at least a digit, special symbol, Uppercase, Lowercase and 8-10 characters long.</p>)}
+        </section>
+
+        <section className="form-flex-1">
+          <label className="form-label">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            className="message-field"
+            placeholder="Send me a message and I will reply you as soon as possible..."
+            {...register("message", { required: true, minLength: 10 })}
+          ></textarea>
+          {errors.message && (<p className="error">Characters too short</p>)}
+        </section>
+
+        <section className="check-box-input">
+          <input
+            type="checkbox"
+            className="check-box"
+            name="checkbox"
+            id="checkbox"
+          />
+          <label className="form-label check-box-label">
+            You agree to providing your data to Divine Edwin, who may contact
+            you.
+          </label>
+        </section>
+
+        <section>
+          <button
+            type="submit"
+            className={Object.keys(errors).length === 0 ? "form-btn-1" : "form-btn"}
+            id="submit_btn"
+          >
+            Send message
+          </button>
+        </section>
+      </form>
+    </>
+  );
+};
+
+export default App;
